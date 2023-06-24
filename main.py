d80927fcce79218d7338b5f92765d51b4ec23532
Fia-Donkor Prince <prince.fia-donkor@kibo.school>
2023-06-24 02:04:06 +0000

    Added a feature that represents data with pie chart as well

--- main.py
+++ main.py
@@ -7,25 +7,25 @@ data = pd.read_csv("waste_data.csv")
 # Group the data by waste type and calculate the total quantity
 grouped_data = data.groupby("Waste Type").agg({"Quantity": "sum"}).reset_index()
 
-# Calculate the total waste quantity
+#Calculate the total waste quantity
 total_waste = grouped_data["Quantity"].sum()
 
-# Calculate the percentage of each waste type
+#Calculate the percentage of each waste type
 grouped_data["Percentage"] = grouped_data["Quantity"] / total_waste * 100
 
-# Analyze plastic waste
+#Analyze plastic wastes
 plastic_waste = grouped_data.loc[grouped_data["Waste Type"] == "Plastics"]
 
-# Print the results
+#Print results
 print("Total Waste Quantity:", total_waste)
 print(plastic_waste)
 print("Plastic Waste Quantity:", plastic_waste["Quantity"].values[0])
 print("Plastic Waste Percentage:", plastic_waste["Percentage"].values[0])
 
-# Save the analysis to a new CSV file
+#store analysis in new CSV file
 grouped_data.to_csv("waste_analysis.csv", index=False)
 
-# Create a bar chart
+#A Bar Chart represenration of data
 bar_chart_data = data.groupby(["Date", "Waste Type"]).agg({"Quantity": "sum"}).reset_index()
 bar_chart_data.pivot(index="Date", columns="Waste Type", values="Quantity").plot(kind="bar", stacked=True)
 plt.xlabel("Date")
@@ -34,8 +34,8 @@ plt.title("Waste Collected by Date and Type")
 plt.legend(title="Waste Type")
 plt.show()
 
-"""# Create a pie chart
+#A Pie Chart represenration of data
 grouped_data.plot(kind="pie", y="Percentage", autopct="%.1f%%", labels=grouped_data["Waste Type"],
             legend=False, title="Total Waste Quantity by Type")
 plt.ylabel("")
-plt.show()"""
+plt.show()
